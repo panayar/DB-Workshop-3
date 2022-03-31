b430f81bd8926fb3a81e0808520c8aff3ec72c47
Paula Anaya <paula.anaya@itac.co>
2022-03-31 17:58:14 -0500

    creating the project

---
+++ mercadolibrebases/src/App.js
@@ -0,0 +1,12 @@
+
+
+
+function App() {
+  return (
+    <div className="App">
+    <p>Hola mundo</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ mercadolibrebases/src/components/loginForm.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class loginForm extends Component {
+  render() {
+    return (
+      <div>loginForm</div>
+    )
+  }
+}
+
+export default loginForm
